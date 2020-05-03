cb06d11ef2ec90fdaaf9422c35ac9486a81afa54
Wei-Hao Wang <wwang03@risd.edu>
2020-05-02 22:07:21 -0400

    cronjob

---
+++ php/config_for_cronjob.php
@@ -0,0 +1,83 @@
+<?php
+date_default_timezone_set('America/New_York');
+
+// database settings
+$db_name = getenv("DATABASE_NAME");
+$db_name = $db_name ? $db_name : "open-records-generator";
+
+
+// $models_root = "/Library/WebServer/Documents/trigger.local/open-records-generator/models/";
+$models_root = "/var/www/html/open-records-generator/models/";
+
+// $lib_root = "/Library/WebServer/Documents/trigger.local/open-records-generator/lib/";
+$lib_root = "/var/www/html/open-records-generator/lib/";
+
+require_once($models_root."model.php");
+require_once($models_root."objects.php");
+require_once($models_root."wires.php");
+require_once($models_root."media.php");
+require_once($models_root."events.php");
+require_once($lib_root."lib.php");
+require_once($lib_root."url-base.php");
+
+set_include_path($lib_root);
+spl_autoload_register(function ($class) {
+	$file = strtolower(preg_replace('#\\\|_(?!.+\\\)#','/', $class) . '.php');
+	if (stream_resolve_include_path($file))
+		require $file;
+});
+
+// connect to database (called in head.php)
+function db_connect($remote_user) {
+	global $adminURLString;
+	global $readOnlyURLString;
+
+	$users = array();
+	$creds = array();
+
+	if ($adminURLString) {
+		// IF YOU ARE USING ENVIRONMENTAL VARIABLES (you should)
+		$urlAdmin = parse_url($adminURLString);
+		$host = $urlAdmin["host"];
+		$dbse = substr($urlAdmin["path"], 1);
+
+		$creds['rw']['db_user'] = $urlAdmin["user"];
+		$creds['rw']['db_pass'] = $urlAdmin["pass"];
+
+		$urlReadOnly = parse_url($readOnlyURLString);
+		$creds['r']['db_user'] = $urlReadOnly["user"];
+		$creds['r']['db_pass'] = $urlReadOnly["pass"];
+
+	} else {
+		// IF YOU ARE NOT USING ENVIRONMENTAL VARIABLES
+		$host = "localhost";
+		$dbse = "trigger_local";
+
+		// full access
+		$creds['full']['db_user'] = "root";
+		$creds['full']['db_pass'] = "f3f4p4ax";
+
+		// read / write access
+		// (can't create / drop tables)
+		$creds['rw']['db_user'] = "root";
+		$creds['rw']['db_pass'] = "f3f4p4ax";
+
+		// read-only access
+		$creds['r']['db_user'] = "root";
+		$creds['r']['db_pass'] = "f3f4p4ax";
+	}
+
+	// users
+	$users["main"] = $creds['rw'];
+	$users["guest"] = $creds['r'];
+	$users["admin"] = $creds['full'];
+
+	$user = $users[$remote_user]['db_user'];
+	$pass = $users[$remote_user]['db_pass'];
+
+	$db = new mysqli($host, $user, $pass, $dbse);
+	if($db->connect_errno)
+		echo "Failed to connect to MySQL: " . $db->connect_error;
+	return $db;
+}
+?>
--- php/update_events.php
+++ php/update_events.php
@@ -1,25 +1,27 @@
 <?
 // require_once('../open-records-generator/config/config');
-$config = $_SERVER['DOCUMENT_ROOT']."/open-records-generator/config/config.php";
+// set_include_path('.');
+$config = "config_for_cronjob.php";
 require_once($config);
 $db = db_connect("guest");
 $oo = new Objects();
 $ww = new Wires();
 
-$upcoming_id = end( $oo->urls_to_ids(array('events', 'upcoming')) );
-$archive_id = end( $oo->urls_to_ids(array('events', 'archive')) );
+$upcoming_id = $oo->urls_to_ids(array('events', 'upcoming'));
+$upcoming_id = end( $upcoming_id );
+$archive_id = $oo->urls_to_ids(array('events', 'archive'));
+$archive_id = end( $archive_id );
 
 $upcoming_events = $oo->children($upcoming_id);
 $now = date('Y-m-d');
-
+// var_dump($upcoming_events);
 foreach( $upcoming_events as $ue ){
 	$this_date = date('Y-m-d', strtotime( $ue['event_date']) );
+	echo $this_date."\n";
 	if( $now > $this_date){
 		$this_id = $ue['id'];
 		$this_wire_id = intval( $ww->get_wire($upcoming_id, $this_id)['id'] );
 		$arr["fromid"] = $archive_id;
 		$ww->update($this_wire_id, $arr);
-	}else{
-		break;
 	}
 }
